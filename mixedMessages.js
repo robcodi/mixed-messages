2b3aff43015d5e7c7607e89b4096ac90cbfd2a0c
Renata Jekk <antalrobika88@gmail.com>
2023-12-18 14:42:56 +0000

    second commit, upgraded version

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,4 +1,8 @@
 const randomJokes =  ['Did you hear they arrested the devil? Yeah, they got him on possession.', 'What did one DNA say to the other DNA? “Do these genes make me look fat?”','My IQ test results came back. They were negative.', 'What do you get when you cross a polar bear with a seal? A polar bear.', 'Why can"t you trust an atom? Because they make up literally everything.' ]
 
-const mixThemUp = Math.floor(Math.random() * 6);
-console.log(randomJokes[mixThemUp])
+for (i=0; i < 7; i++) {
+    const mixThemUp = Math.floor(Math.random() * 5);
+    console.log(randomJokes[mixThemUp])
+}
+
+
